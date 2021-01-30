c6b221b3d0419183111e97c3148a115c00bcdf87
surajit mirbar <surajitmirbar980@gmail.com>
2021-01-30 17:06:35 +0530

    error message for chose bigger number to 100

--- guessinggame.py
+++ guessinggame.py
@@ -1,14 +1,22 @@
 #guessing game 
-
+import os
 import random          #import random libary for taking random number
 jacpot=random.randint(1,100)       #random number genarate in jackpot variable
-guess=int(input("guess your number :"))     # for taking user guess in guess variable
-counter=1
-while guess!=jacpot:                         # loops use for taking multiple input when guess is wrong 
-    if guess>jacpot:
-        print("your guess is very high guess low..")
-    else:
-         print("your guess is very low guess haigh...")
-    guess=int(input("re guess your number :"))      # taking user re guess number
-    counter += 1
-print("congratulation your guess is right and you taking",counter,"step")
+def guessing_game():
+     guess=int(input("guess your number between 1 to 100 :"))     # for taking user guess in guess variable
+     if guess<=100:
+         os.system("cls")
+         print("now you are in game")
+         counter=1
+         while guess!=jacpot:                         # loops use for taking multiple input when guess is wrong 
+            if guess>jacpot:
+                print("your guess is very high guess low..")
+            else:
+                print("your guess is very low guess haigh...")
+            guess=int(input("re guess your number :"))      # taking user re guess number
+            counter += 1
+         print("congratulation your guess is right and you taking",counter,"step")
+     else:
+         print("you need to guess numbers between 1 to 100..sorry please try again......")
+         guessing_game()
+guessing_game()
